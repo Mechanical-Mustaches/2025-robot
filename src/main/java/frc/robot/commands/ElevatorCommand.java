45181ee6522f7bc006ed09a68fb6d211a6231eeb
rcbassie <rcbass27@gmail.com>
2025-03-12 15:27:56 -0500

    safety for elevator

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -4,39 +4,30 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.EndEffectorSubsystem;
 
-public class ElevatorCommand extends Command{
+public class ElevatorCommand extends Command {
     private final ElevatorSubsystem elevator;
     private ElevatorSubsystem.Level targetLevel;
-    EndEffectorSubsystem endEffector; 
-    private boolean isIntakingAlgae;
+    EndEffectorSubsystem endEffector;
 
-    public ElevatorCommand(ElevatorSubsystem subsystem, ElevatorSubsystem.Level level, EndEffectorSubsystem endEffector, boolean isIntakingAlgae){
+    public ElevatorCommand(ElevatorSubsystem subsystem, ElevatorSubsystem.Level level, EndEffectorSubsystem endEffector,
+            boolean isIntakingAlgae) {
         elevator = subsystem;
         targetLevel = level;
         this.endEffector = endEffector;
-        this.isIntakingAlgae = isIntakingAlgae;
     }
 
     @Override
     public void initialize() {
-        if(!isIntakingAlgae){
-        elevator.setPosition(targetLevel, endEffector);
-        } else{
-            elevator.algaeDescent();
+        if (endEffector.isCoralSeenBack()) {
+            return;
         }
+
+        elevator.setPosition(targetLevel, endEffector);
     }
-  
-   
-    @Override
-    public void end(boolean interrupted) {
-        //elevator.stopElevator();
-    }
-  
-    
+
     @Override
     public boolean isFinished() {
-      return true;
-        //TODO: return value based on encoder value, especially for sequential commands.
+        return true;
     }
 
 }
